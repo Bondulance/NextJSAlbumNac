ce97b2398f3a9ac45d91323bbf8924756cfdee85
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-04-24 18:31:34 -0400

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: [
+    "/app/api/webhook",
+    "/api/webhooks(.*)",
+    "/",
+    "/api/webhook",
+    "/community",
+  ],
+  ignoredRoutes: ["/api/webhook", "/newrank"],
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
