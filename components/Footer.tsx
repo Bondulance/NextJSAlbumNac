d0bdcc897a7ec55a86ad498e45d27ba2d666fe8c
Bodhi Woll <144378333+Bondulance@users.noreply.github.com>
2024-04-25 09:44:04 -0400

    yay

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer but cool af hell yeah</div>;
+  return <div>Footer but cool af hell yeah Hekc yes</div>;
 };
 
 export default Footer;
